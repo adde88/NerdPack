7207890c2cf6827ddd30fbfa0eadd1dbf8d0574d
Jaime Marques <mrthesoulz@gmail.com>
2017-09-28 21:53:39 +0100

    version bump

--- NerdPack.lua
+++ NerdPack.lua
@@ -1,7 +1,7 @@
 local n_name, NeP = ...
 
-NeP.Version = 1.9
-NeP.Branch  = 'Release1'
+NeP.Version = 1.10
+NeP.Branch  = 'Dev1'
 NeP.Media   = 'Interface\\AddOns\\' .. n_name .. '\\Media\\'
 NeP.Color   = 'FFFFFF'
 
